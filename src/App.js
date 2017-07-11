81541cc73386a975791884c2a286ab0e7b2892df
choi hoon chul <hosoon2@gmail.com>
2017-07-11 15:18:33 +0900

    socket.io 연결 위치 변경
    SPA 구조라 첫화면에서 socket연결하고 socket id를 redux 로 관리할 예정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Login, RoomList, RoomView } from './containers';
 
+import SocketIOClient from 'socket.io-client';
+
 class App extends Component {
+    // Creating the socket-client instance will automatically connect to the server.
+    socket = SocketIOClient('http://localhost:3000');
+
+    componentDidMount() {
+        this.socket.on('init', (data) => {
+            console.log('init /  ' , data);
+        });
+    }    
+
     render() {
         return (
             <div>
