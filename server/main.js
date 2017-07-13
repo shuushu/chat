4911881fe70e53d03d2298352abd29eb6add8ca1
choi hoon chul <hosoon2@gmail.com>
2017-07-13 17:53:12 +0900

    body-parser 라이브러리
     미들웨어에서 모든 request을 body-parser로 처리한다

--- server/main.js
+++ server/main.js
@@ -1,12 +1,17 @@
 let express = require('express');
 let session = require('express-session');
 let http = require('http');
+let bodyParser = require('body-parser');
 
 let app = express();
 let server = http.Server(app);
 /* setup routers & static directory */
 let api = require('./routes');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/api', api);
+
 
 // Server ON:3500
 server.listen(3500, () => console.log('listening on *:3500'));
@@ -17,6 +22,3 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
-
-
-
