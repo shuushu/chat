1fcd0ff6c80f272b1ddf05bda918c6dbf23188d2
choi hoon chul <hosoon2@gmail.com>
2017-08-02 17:05:31 +0900

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { RoomList, RoomView, Login } from './index';
+import SocketIOClient from 'socket.io-client';
+
+let socket = SocketIOClient('/roomView');
 
 class App extends Component {
     render() {
@@ -10,7 +13,8 @@ class App extends Component {
                     <Route exact path="/" component={RoomList} />
                     <Route exact path="/Login" component={Login} />
                     <Route path="/RoomList" component={RoomList} />
-                    <Route path="/roomView/:user" component={RoomView} />
+                    <Route path="/roomView/:user" render={(path) => ( <RoomView rpath={path} socket={socket} /> )}  />
+
                 </Switch>
             </div>
         )
