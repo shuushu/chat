9349997a5952b8f0dd2c06e198c44de9c29b5ee1
shushu <hosun2da@gmail.com>
2017-09-13 18:31:42 +0900

    1) 스크롤 이벤트 > 메세지 더 보기
    2) 첫렌더링 > 최근 메세지 10건
    
    3) 채팅룸 > 새메세지 받았을 때 > 화면이 위에 있을때  메시지 창 노출, 하단일 경우 그냥 스크롤이벤트

--- src/containers/RoomView.js
+++ src/containers/RoomView.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import '../scss/roomView.css';
 import { firebaseConnect, pathToJS, dataToJS } from 'react-redux-firebase';
 import {convertDate} from '../commonJS/Util';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 @firebaseConnect((props) => {
     return [
@@ -24,20 +25,55 @@ import {convertDate} from '../commonJS/Util';
 )
 
 class App extends Component {
-    state = {
-        redirect: false,
-        ieEmpty: false,
-        roomViewData: null,
-        latestMsg: '',
-        newMsg: false
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            redirect: false,
+            ieEmpty: false,
+            roomViewData: null,
+            latestMsg: '',
+            hasNewMessage: false,
+            size: 10
+        };
+        this.onScroll = this.onScroll.bind(this);
+    }
+
 
     componentDidMount() {
         setTimeout(()=>{
             window.scrollTo(0, document.body.scrollHeight);
         }, 100);
 
-        //this.setState({ newMsg: true });
+        window.addEventListener('scroll', this.onScroll, true);
+
+        let timer;
+        let repeat = () => {
+            if(timer){
+                clearTimeout(timer);
+            }
+            // firebase에 연결될때까지 요청
+            if(this.props.message !== undefined) {
+                // 요청되면 타이머 종료
+                clearTimeout(timer);
+
+                let that = this;
+                let msgSize = that.props.message[that.props.rpath.match.params.user].length-1;
+
+                this.props.firebase.ref('/message/' + this.props.rpath.match.params.user).limitToLast(1).on("child_added", function(snapshot) {
+                    // 컴포넌트 렌더링 이후 메세지 수신이 있으면 실행
+                    if(msgSize !== Number(snapshot.key)) {
+                        if(snapshot.val().uid !== that.props.auth.uid) {
+                            that.setState({ hasNewMessage: true })
+                        }
+                    }
+                });
+            } else {
+                timer = setTimeout(repeat, 1000);
+            }
+        };
+
+        repeat();
     }
 
     componentWillReceiveProps ({ auth, room }) {
@@ -53,41 +89,31 @@ class App extends Component {
             })
         }
     }
+    // 마운트해제
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.onScroll, true);
+    }
+    
 
-    onScroll(lastMsg) {
+    onScroll() {
         const scrollTop = document.body.scrollTop;
         const clientHeight = document.body.clientHeight;
         const screenHeight = window.screen.height;
 
+        if ( scrollTop < 100) {
+            this.handleLoad();
+        }
+
         if ( scrollTop >= clientHeight - screenHeight ){
-            console.log(lastMsg)
+            //console.clear();
         } else {
 
-            /*if(lastMsg.uid === this.props.auth.uid) {
-
-            } else {
-
-            }*/
         }
     }
 
-    scrollToBottom() {
-        setTimeout(()=>{
-            window.scrollTo(0, document.body.scrollHeight);
-        }, 100);
-
-        this.setState({
-            isScrollAtBottom: true,
-            hasNewMessage: false
-        });
-    }
-
-
     // input TEXT
     handleChange = (e) => {
-        this.setState({
-            latestMsg: e.target.value
-        })
+        this.setState({ latestMsg: e.target.value })
     };
 
     handleSubmit = (e) => {
@@ -117,13 +143,23 @@ class App extends Component {
         let that = this;
 
         this.props.firebase.ref(`/message/${KEY}`).update(msg, function(){
-            /*let msg = that.props.message[that.props.rpath.match.params.user];
-
-            that.onScroll(msg[msg.length-1]);*/
-
             window.scrollTo(0, document.body.scrollHeight);
-            that.setState({ latestMsg: '' });
+            that.setState({
+                latestMsg: '',
+                size: that.state.size + 1
+            });
         });
+    };
+
+    handleLoad = (e) => {
+        if(e) {
+            e.preventDefault();
+        }
+
+        let max = this.props.message[this.props.rpath.match.params.user].length;
+        let current = (this.state.size + 10 >= max) ? max : this.state.size + 10;
+
+        this.setState({ size: current });
     };
 
     render() {
@@ -158,6 +194,8 @@ class App extends Component {
                 let msgData = message[key];
 
                 return msgData.map((data,i) => {
+                    if(i < msgData.length - this.state.size){ return true }
+
                     return (
                         <div key={`itemMSG${i}`} className={data.uid === this.props.auth.uid ? 'mine' : 'list'}>
                             <div className="imgs">
@@ -180,11 +218,35 @@ class App extends Component {
         let test = (newMsg) => {
             //if(newMsg) {
                 let msgArr = this.props.message[this.props.rpath.match.params.user];
-                console.log(msgArr[msgArr.length-1]);
+                //console.log(msgArr[msgArr.length-1]);
             //}
         };
 
-        // {mapToList(this.state.roomViewData)}
+        // 새메세지가 왔을때
+        let newMsgRender = () => {
+            const scrollTop = document.body.scrollTop;
+            const clientHeight = document.body.clientHeight;
+            const screenHeight = window.screen.height;
+
+            // 화면이 하단에 있을때
+            if ( scrollTop >= clientHeight - screenHeight ){
+                this.setState({ hasNewMessage: false });
+                window.scrollTo(0, clientHeight);
+            } else {
+                // 화면이 상단에 있음
+                setTimeout(() => {
+                    this.setState({ hasNewMessage: false });
+                }, 2000);
+
+                return (
+                    <button className="animated message__unread fadeIn waves-effect waves-light btn">
+                        새 메시지가 있습니다.
+                        <i className="material-icons right">new_releases</i>
+                    </button>
+                );
+            }
+        };
+
         return (
               <div className="App">
                   <div>
@@ -198,13 +260,17 @@ class App extends Component {
                   </div>
                   {this.props.message && test(this.state.newMsg)}
 
-                  {this.state.hasNewMessage &&
-                      <button className="animated message__unread fadeIn waves-effect waves-light btn">
-                          새 메시지가 있습니다.
-                          <i className="material-icons right">new_releases</i>
-                      </button>
-                  }
-
+                  <CSSTransitionGroup
+                      component="div"
+                      className="animated"
+                      transitionName={{
+                          enter: 'fadeIn',
+                          leave: 'fadeOutDown'
+                      }}
+                      transitionAppearTimeout={3000}
+                      transitionLeaveTimeout={1000}>
+                      {this.state.hasNewMessage && newMsgRender()}
+                  </CSSTransitionGroup>
 
                   <div className="msgSendForm">
                       <form action="" onSubmit={this.handleSubmit}>
